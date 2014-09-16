1d4ffaf8d3e3528a697c57cd060b868906c72a4a
DerZyklop <mail@der-zyklop.de>
2014-09-16 18:24:43 +0200

    added Pygments; added todo-echo; added 1password-download

--- init.sh
+++ init.sh
@@ -38,6 +38,9 @@ read -p "${green}Wanna install commands via brew?${reset} [yN] " -n 1 -r
 echo "\nAllright!"
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   source $DOTFILESDIR/init/initbrew.sh
+  # If you're wondering, easy_install is a Python module used for managing Python packages
+  # Pygments is needed for beatiful colored `cat`
+  sudo easy_install Pygments
 fi
 
 mkdir ~/todo
@@ -68,4 +71,19 @@ if [ -f ~/.gitignore ]; then
 fi
 # For more usefull git-stuff see <http://git-scm.com/book/en/Customizing-Git-Git-Configuration>
 
+read -p "${green}Should i install 1Password?${reset} [yN] " -n 1 -r
+echo "\nAllright!"
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  cd ~/Downloads/
+  wget https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-4.4.1.zip
+  unzip 1Password-4.4.1.zip -d /Applications
+  rm ~/Downloads/1Password-4.4.1.zip
+  read -p "${purple}Set up 1Password, and then press [Enter] key to contunue...${reset}"
+fi
+
 open $DOTFILESDIR/init/DerZyklop.terminal
+
+echo "${purple}Your todos now:${reset}"
+echo "${purple}-${reset} Set up Bittorrent Sync \`o ~/Applications/BitTorrent\ Sync.app\`"
+echo "${purple}-${reset} run \`brew cask info little-snitch\`"
+echo "${purple}-${reset} run \`brew cask info default-folder-x\`"
