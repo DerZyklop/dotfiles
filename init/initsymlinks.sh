59a668fda60835fecf48982a0a5b4cf4dcd3657f
DerZyklop <derzyklop@BlackBook.local>
2014-09-16 16:51:25 +0200

    improved symlink-creation

--- init/initsymlinks.sh
+++ init/initsymlinks.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ln -sf $DOTFILESDIR/.path ~
-ln -sf $DOTFILESDIR/.completions ~
-ln -sf $DOTFILESDIR/.bash_prompt ~
-ln -sf $DOTFILESDIR/.aliases ~
-ln -sf $DOTFILESDIR/.functions ~
-ln -sf $DOTFILESDIR/.profile ~
-ln -sf $DOTFILESDIR/.gitignore ~
-ln -sf $DOTFILESDIR/.exports ~
+ln -sf $DOTFILESDIR/.path $HOME
+ln -sf $DOTFILESDIR/.completions $HOME
+ln -sf $DOTFILESDIR/.bash_prompt $HOME
+ln -sf $DOTFILESDIR/.aliases $HOME
+ln -sf $DOTFILESDIR/.functions $HOME
+ln -sf $DOTFILESDIR/.profile $HOME
+ln -sf $DOTFILESDIR/.gitignore $HOME
+ln -sf $DOTFILESDIR/.exports $HOME
