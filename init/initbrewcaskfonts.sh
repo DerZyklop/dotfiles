4413398c5638b887093edf677a8cf08a3c60a51e
DerZyklop <mail@der-zyklop.de>
2015-01-19 11:47:31 +0100

    added titillium

--- init/initbrewcaskfonts.sh
+++ init/initbrewcaskfonts.sh
@@ -5,20 +5,21 @@
 brew tap caskroom/fonts
 
 HOMEBREW_CASK_FORMULAE=(
-    "font-amaranth"
-    "font-droid-sans-mono"
-    "font-droid-sans"
-    "font-droid-serif"
-    "font-merriweather"
-    "font-source-code-pro"
-    "font-fira-sans"
-    "font-league-gothic"
-    "font-bitter"
-    "font-vollkorn"
     "font-alegreya"
     "font-alegreya-sc"
     "font-alegreya-sans"
     "font-alegreya-sans-sc"
+    "font-amaranth"
+    "font-bitter"
+    "font-droid-sans-mono"
+    "font-droid-sans"
+    "font-droid-serif"
+    "font-fira-sans"
+    "font-league-gothic"
+    "font-merriweather"
+    "font-source-code-pro"
+    "font-titillium-web"
+    "font-vollkorn"
 )
 
 for i in ${!HOMEBREW_CASK_FORMULAE[*]}; do
