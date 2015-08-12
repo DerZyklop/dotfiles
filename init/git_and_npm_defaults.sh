b40699aedc5cfef31065076e821d8a08254c7061
DerZyklop <mail@der-zyklop.de>
2015-08-12 14:55:45 +0200

    added git globals

--- init/git_and_npm_defaults.sh
+++ init/git_and_npm_defaults.sh
@@ -27,3 +27,23 @@ npm config set init.author.name "${npmusername:-$npmusernamedefault}"
 npm config set init.author.email "${npmuseremail:-$npmuseremaildefault}"
 npm config set init.author.url "${userurl:-$urldefault}"
 npm config set init.license "${userlicense:-$userlicensedefault}"
+
+# Define a global .gitignore file.
+git config --global core.excludesfile '~/.gitignore'
+
+# Erlaube Git Kommandos farbige Ausgaben, falls möglich
+git config --global color.ui auto
+
+# Good explanation of git's fast forwarding: http://ariya.ofilabs.com/2013/09/fast-forward-git-merge.html
+
+# Deaktiviere die Belehrungen durch Git wenn Du etwas pushen möchtest was nicht fast forwardable ist
+git config --global advice.pushNonFastForward false
+
+# Setzte no fast-forward als Standardeinstellung
+git config branch.master.mergeoptions  "--no-ff"
+
+# Deaktiviere die “how to stage/unstage/add” Tips durch git status:
+git config --global advice.statusHints false
+
+# Erlaube git diffdie Erkennung einfacher Umbenennungen und Kopien:
+git config --global diff.renames copies
