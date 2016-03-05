22eaa58c1bd62cee47ee4e4472323e467b9a088d
DerZyklop <mail@der-zyklop.de>
2016-03-05 14:41:35 +0100

    added chown for brew to init.sh

--- init.sh
+++ init.sh
@@ -15,6 +15,9 @@ echo "${reset}"
 # Ask for the administrator password upfront
 sudo -v
 
+sudo chown -R $USER /usr/local
+sudo chown -R $USER /Library/Caches/Homebrew/
+
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
