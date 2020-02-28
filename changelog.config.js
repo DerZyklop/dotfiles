2fbfbffb53556be1199709338f1509ae8913e092
DerZyklop <mail@der-zyklop.de>
2020-02-28 14:33:51 +0100

    feat(config): back to cz-conventional-changelog

--- changelog.config.js
+++ changelog.config.js
@@ -1,46 +1,46 @@
-module.exports = {
-  "disableEmoji": false,
-  "list": [
-    "fix",
-    "improve",
-    "feat",
-    "other",
-  ],
-  "maxMessageLength": 64,
-  "minMessageLength": 3,
-  "questions": [
-    "type",
-    "scope",
-    "subject",
-    "body",
-    "breaking",
-    "issues",
-    "lerna"
-  ],
-  "scopes": [
-    'config',
-    'other',
-  ],
-  "types": {
-    "fix": {
-      "description": "A bug fix",
-      "emoji": "🐛",
-      "value": "fix"
-    },
-    "improve": {
-      "description": "A improvement",
-      "emoji": "💅",
-      "value": "improve"
-    },
-    "feat": {
-      "description": "A new feature",
-      "emoji": "✨",
-      "value": "feat"
-    },
-    "other": {
-      "description": "Other",
-      "emoji": "…",
-      "value": "other"
-    },
-  }
-};
+// module.exports = {
+//   "disableEmoji": false,
+//   "list": [
+//     "fix",
+//     "improve",
+//     "feat",
+//     "other",
+//   ],
+//   "maxMessageLength": 64,
+//   "minMessageLength": 3,
+//   "questions": [
+//     "type",
+//     "scope",
+//     "subject",
+//     "body",
+//     "breaking",
+//     "issues",
+//     "lerna"
+//   ],
+//   "scopes": [
+//     'config',
+//     'other',
+//   ],
+//   "types": {
+//     "fix": {
+//       "description": "A bug fix",
+//       "emoji": "🐛",
+//       "value": "fix"
+//     },
+//     "improve": {
+//       "description": "A improvement",
+//       "emoji": "💅",
+//       "value": "improve"
+//     },
+//     "feat": {
+//       "description": "A new feature",
+//       "emoji": "✨",
+//       "value": "feat"
+//     },
+//     "other": {
+//       "description": "Other",
+//       "emoji": "…",
+//       "value": "other"
+//     },
+//   }
+// };
