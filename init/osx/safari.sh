b99c74e2acf056b00ec24ec7d6f57b5972db39fb
Nils <nils@dr-plano.com>
2022-01-21 21:32:00 +0100

    NO_JIRA_ISSUE ✨ added safari.sh

---
+++ init/osx/safari.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Safari                                                               #
+###############################################################################
+
+defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
